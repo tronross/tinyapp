c8be9db8f7664404e12df753b4a828b03483b518
tronross <john_ross@analogforever.ca>
2022-10-29 16:20:16 -0400

    Add render for individual URL & shortened-URL page

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", tempVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const tempVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", tempVars);
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
