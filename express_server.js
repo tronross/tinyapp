d8bc6847b04a33645c4c1d3984d131795b5228af
tronross <john_ross@analogforever.ca>
2022-10-29 19:16:25 -0400

    Add test code to log random string to console on page refresh

--- express_server.js
+++ express_server.js
@@ -34,8 +34,8 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(req.body, genRanStr()); // Log the POST request body to the console
+  res.send('ok'); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
