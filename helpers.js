9a7904990a9b0d47419c030ac45dbcabbfb2d203
tronross <john_ross@analogforever.ca>
2022-11-05 15:53:55 -0400

    Refactor as modularized helper functions

--- helpers.js
+++ helpers.js
@@ -1,4 +1,7 @@
+////////////////////////////////////////////
 // HELPER FUNCTIONS
+////////////////////////////////////////////
+
 // getUserByEmail: check email against emails in users database
 const getUserByEmail = function(email, database) {
   for (const user in database) {
@@ -8,7 +11,36 @@ const getUserByEmail = function(email, database) {
       return validUser;
     }
   }
-  return null;
 };
 
-module.exports = { getUserByEmail };
+
+// genRanStr: generate random six character alphanumeric string
+const genRanStr = function() {
+  const charSet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const charSetLength = charSet.length;
+  let randString = '';
+  
+  for (let i = 0; i < 6; i++) {
+    const randIndex = Math.floor(Math.random() * charSetLength);
+    const randChar = charSet.charAt(randIndex);
+    randString += randChar;
+  }
+  return randString;
+};
+
+
+// urlsForUser: search input database and return database of shortURL-longURL pairs belonging to the input userID
+const urlsForUser = function(id, database) {
+  const userURLs = {};
+  for (const shortUrl in database) {
+    const longUrl = database[shortUrl].longURL;
+    const user = database[shortUrl].userID;
+    if (id === user) {
+      userURLs[shortUrl] = longUrl;
+    }
+  }
+  return userURLs;
+};
+
+
+module.exports = { getUserByEmail, genRanStr, urlsForUser };
